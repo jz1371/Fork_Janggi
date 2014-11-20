f0b62dbae9a1e483bdb882f135103f32fc9ce5c7
sh2514 <shu0018@live.com>
2014-11-19 20:39:16 -0500

    Changed sound playing method

--- game.js
+++ game.js
@@ -6,8 +6,8 @@ angular.module('myApp')
       $window, $scope, $log, $animate, $timeout,
       gameService, scaleBodyService, gameLogic) {
 
-      var moveAudio = new Audio('audio/move.wav');
-      moveAudio.load();
+      //var moveAudio = new Audio('audio/move.wav');
+      //moveAudio.load();
     
         // USED FOR ANIMATIONS
       $scope.animatePiece;
@@ -551,7 +551,8 @@ angular.module('myApp')
         }
         else
         {
-            moveAudio.play();
+            //moveAudio.play();
+            document.getElementById('sound1').play();
         }
         $scope.isYourTurn = params.turnIndexAfterMove >= 0 &&
             params.yourPlayerIndex === params.turnIndexAfterMove;
